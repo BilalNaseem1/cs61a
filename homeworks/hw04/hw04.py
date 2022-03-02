eb457fce66eda09657fd3b9a3e93b46d72d90ecb
tomthestrom <thruska29@gmail.com>
2022-03-02 12:45:10 +0100

    hw04 q5

--- homeworks/hw04/hw04.py
+++ homeworks/hw04/hw04.py
@@ -267,6 +267,12 @@ def remove_all(link , value):
     <0 1>
     """
     "*** YOUR CODE HERE ***"
+    while link.rest is not Link.empty:
+        if link.rest.first != value:
+            link = link.rest
+        else:
+            link.rest = link.rest.rest
+
 
 def generate_paths(t, x):
     """Yields all possible paths from the root of t to a node with the label x
