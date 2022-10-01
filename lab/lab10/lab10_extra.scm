d4a89877228660d78eff741539aa08805f0ad2bf
tomthestrom <thruska29@gmail.com>
2022-10-01 23:02:12 +0200

    lab10 opt08 no-repeats done

--- lab/lab10/lab10_extra.scm
+++ lab/lab10/lab10_extra.scm
@@ -1,5 +1,20 @@
 ;; Scheme ;;
 
+;helper
+(define (filter-lst f lst)
+  (cond
+    ((null? lst) nil)
+    ((f (car lst)) (cons (car lst) (filter-lst f (cdr lst))))
+    (else (filter-lst f (cdr lst)))
+    )
+)
+(define (is-in-lst item lst)
+  (cond
+    ((null? lst) #f)
+    ((= item (car lst)) #t)
+    (else (is-in-lst item (cdr lst)))
+    )
+  )
 
 (define lst
  '((1) 2 (3 4) 5)
@@ -10,12 +25,9 @@
 )
 
 (define (remove item lst)
-  (cond
-    ((null? lst) nil)
-    ((= item (car lst)) (remove item (cdr lst)))
-    (else (cons (car lst) (remove item (cdr lst))))
-    )
+  (filter-lst (lambda (x) (not (= item x))) lst)
 )
+
 
 
 ;;; Tests
@@ -26,10 +38,17 @@
 (remove 5 '(5 3 5 5 1 4 5 4))
 ; expect (3 1 4 4)
 
-(define (no-repeats s)
-  'YOUR-CODE-HERE
-)
 
+
+(define (no-repeats s)
+    (
+        cond 
+                 ((null? s) nil)
+                 ((is-in-lst (car s) (cdr s)) (cons (car s) (no-repeats (remove (car s) (cdr s)))))
+                 (else (cons (car s) (no-repeats (cdr s))))
+                 
+    )    
+)
 (define (substitute s old new)
   'YOUR-CODE-HERE
 )
