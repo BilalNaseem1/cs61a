0871bab0475809afc75075bba8dcd3ffc0a90abe
tomthestrom <thruska29@gmail.com>
2022-10-02 00:05:43 +0200

    lab10 opt09 substitute done

--- lab/lab10/lab10_extra.scm
+++ lab/lab10/lab10_extra.scm
@@ -49,8 +49,15 @@
                  
     )    
 )
+
 (define (substitute s old new)
-  'YOUR-CODE-HERE
+  (
+    cond 
+        ((null? s) nil)
+        ((pair? (car s))(cons (substitute (car s) old new) (substitute (cdr s) old new)))
+        ((eq? old (car s))(cons new (substitute (cdr s) old new)))
+        (else (cons (car s) (substitute (cdr s) old new)))
+   )
 )
 
 
