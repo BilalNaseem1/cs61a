029643d51a7f53ef6592e4cfb6ce1b80a7b7df1a
tomthestrom <thruska29@gmail.com>
2022-10-20 13:53:23 +0200

    lab13 q4

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -7,7 +7,10 @@ CREATE TABLE smallest_int AS
   SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest ASC LIMIT 20;
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT f.pet, f.song, f.color, s.color FROM students f, students s
+   WHERE f.pet = s.pet            
+   AND f.song = s.song
+   AND f.time < s.time; ;
 
 CREATE TABLE smallest_int_having AS
   SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
