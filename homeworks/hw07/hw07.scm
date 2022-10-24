08e2fea8f8fbb9149f4b4da8ca1fbec2b36ac847
tomthestrom <thruska29@gmail.com>
2022-10-24 17:07:07 +0200

    hw07 q2

--- homeworks/hw07/hw07.scm
+++ homeworks/hw07/hw07.scm
@@ -7,6 +7,16 @@
   (cons-stream 3 (map-stream (lambda (x) (+ x 3)) multiples-of-three))
 )
 
+
 (define (rle s)
-  'YOUR-CODE-HERE
+    (define (compress strm prev count)
+        (if (or (null? strm) (not (= prev (car strm))))
+            (cons-stream (list prev count) (rle strm))
+            (compress (cdr-stream strm) prev (+ 1 count))
+        )
+    )
+    
+    (if (null? s)
+        nil
+        (compress  (cdr-stream s) (car s) 1))
 )
